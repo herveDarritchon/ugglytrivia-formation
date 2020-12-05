f88cfece6f72d4c558484645081b5ba07ac8ef7a
Clément Bouillier <clement.bouillier@devcrafting.com>
2020-12-05 15:14:10 +0100

    init Java version with updated Java testing libs : JUnit 5 & assertj

---
+++ java/readme.md
@@ -0,0 +1,7 @@
+# Java setup
+
+It expects at least Java 8 SDK.
+
+This project uses Maven.
+
+You have JUnit 5 and JAssert available.
--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,13 +1,15 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
+	public void true_is_true() {
+		assertTrue(false); // JUnit 5 assertions
+		assertThat(false).isTrue(); // JAssert
 	}
 }
